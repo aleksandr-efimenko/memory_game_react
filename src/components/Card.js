2c6a7a2a15329162e9680aae580b7064e28a8254
androidblog <androidblog@yandex.ru>
2023-01-12 13:41:09 +0400

    Create card component with ability to flip and default picture

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Educative from '../Images/EducativeIcon.png';
 
-export default function Card() {
+export default function Card({ card, flipped, disabled, handleChoice }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  }
   return (
-    <div>Card</div>
+    <div className="card" key={card.id}>
+      <div className={flipped ? "flipped" : ""}>
+        <img 
+        src={Educative} 
+        onClick={handleClick}
+        alt="card back"
+        className="back" />
+      </div>
+    </div>
   )
 }
