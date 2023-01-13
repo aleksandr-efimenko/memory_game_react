7747451caa181d7f9517b9699cb408d5df789ba7
androidblog <androidblog@yandex.ru>
2023-01-13 12:03:44 +0400

    create css for grid and cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { cardImages } from './Components/Images';
 import Header from './Components/Header';
+import Grid from './Components/Grid';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // Handle Choice by adding the clicked cards in the two slot state defined
   const handleChoice = (card) => {
@@ -31,6 +33,13 @@ function App() {
   return (
     <div className="App">
       <Header turns={turns} onShuffle={shuffleCards} />
+      <Grid 
+        cards={cards}
+        choiceOne={choiceOne}
+        choiceTwo={choiceTwo}
+        disabled={disabled}
+        handleChoice={handleChoice}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,14 +8,16 @@ export default function Card({ card, flipped, disabled, handleChoice }) {
     }
   }
   return (
-    <div className="card" key={card.id}>
+    // <div className="card" key={card.id}>
       <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className='front' />
         <img 
-        src={Educative} 
-        onClick={handleClick}
-        alt="card back"
-        className="back" />
+          src={Educative} 
+          onClick={handleClick}
+          alt="card back"
+          className="back" 
+        />
       </div>
-    </div>
+    // </div>
   )
 }
