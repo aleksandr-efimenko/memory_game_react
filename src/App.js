eaf9ac306b146ae5bc0666142b2cf3dc697f1b11
androidblog <androidblog@yandex.ru>
2023-01-13 12:59:17 +0400

    check winner after 15 turns

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  
+  //check winner
+  const [winner, setWinner] = useState(null);
+  const [exceeds, setExceeds] = useState(null);
 
   // Handle Choice by adding the clicked cards in the two slot state defined
   const handleChoice = (card) => {
@@ -23,8 +27,14 @@ function App() {
       .map((card) => ({ ...card, id: Math.random() }));
       setCards(shuffledCards);
       setTurns(0);
+      setWinner(false);
+      setExceeds(false);
+      setDisabled(false);
+      setChoiceOne(null);
+      setChoiceTwo(null);
   }
 
+  // Reset after checking 2 cards
   const backToDefault = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
@@ -37,6 +47,7 @@ function App() {
     shuffleCards();
   }, [])
 
+  // Check if the two cards clicked are matching
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       setDisabled(true);
@@ -57,9 +68,26 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
+  useEffect(() => {
+    setTimeout(() => {
+      const allMatched = cards.every((card) => card.matched === true);
+      if (turns >= 15) {
+        if (allMatched && turns === 15) {
+          setWinner(true);
+        } else {
+          setExceeds(true);
+          setDisabled(true);
+        }
+      } else if (allMatched && cards.length > 0) {
+        setWinner(true);
+      }
+    }, 500);
+  }, [turns, cards, winner])
   return (
     <div className="App">
       <Header turns={turns} onShuffle={shuffleCards} />
+      {winner ? <div className='result'>Congratulations, You win!!</div> : <></>}
+      {exceeds ? <div className='result'>You are out of turns!:(</div> : <></>}
       <Grid 
         cards={cards}
         choiceOne={choiceOne}
